377011e5facebd9b325f90578a2b62309eb99a15
AnHyeongryeol <reversiblehope@gmail.com>
2024-05-09 17:47:07 +0900

    SpringBoot BLOG CRUD Dev

--- src/main/java/org/second/springbootdeveloper/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package org.second.springbootdeveloper;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
---
+++ src/main/java/org/second/springbootdeveloper/repository/BlogRepository.java
@@ -0,0 +1,8 @@
+package org.second.springbootdeveloper.repository;
+
+import org.second.springbootdeveloper.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+
+}
