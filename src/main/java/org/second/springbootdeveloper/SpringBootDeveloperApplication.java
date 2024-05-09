7a838ada61ce17205f582098294624fec57b341d
AnHyeongryeol <reversiblehope@gmail.com>
2024-05-09 15:06:58 +0900

    SpringBoot 구조 이해하기

--- src/main/java/org/second/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/org/second/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -1,2 +1,12 @@
-package org.second.springbootdeveloper;public class SpringBootDeveloperApplication {
+package org.second.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootDeveloperApplication.class, args);
+  }
+
 }
--- src/main/java/org/second/springbootdeveloper/TestController.java
+++ src/main/java/org/second/springbootdeveloper/TestController.java
@@ -1,2 +1,20 @@
-package org.second.springbootdeveloper;public class TestController {
+package org.second.springbootdeveloper;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+  @Autowired
+  TestService testService;
+
+  @GetMapping("/test")
+  public List<Member> getAllMember() {
+    List<Member> members = testService.getAllMembers();
+    return members;
+  }
+
 }
