1b3982dfea8d1bd61346b780595bf63f741c83cd
AnHyeongryeol <reversiblehope@gmail.com>
2024-05-10 08:05:15 +0900

    삭제, 수정 기능 완성

--- src/main/java/org/second/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/org/second/springbootdeveloper/controller/BlogViewController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -34,4 +35,15 @@ public class BlogViewController {
     return "article";
   }
 
+  @GetMapping("/new-article")
+  // id 키를 가진 쿼리 파라미터의 값을 id 변수에 매핑
+  public String newAtrticle(@RequestParam(required = false) Long id, Model model) {
+    if (id == null) { // id가 없으면 생성
+      model.addAttribute("articles", new ArticleViewResponse());
+    } else {
+      Article article = blogService.findById(id);
+      model.addAttribute("article", new ArticleViewResponse(article));
+    }
+    return "newArticle";
+  }
 }
